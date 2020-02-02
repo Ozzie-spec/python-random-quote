52c81290c4083e404ef9540bbd815041b25d0eae
Ozzie <alexozolins@hotmail.com>
2020-02-02 17:47:02 +1100

    Added extra stuff

--- get-quote.py
+++ get-quote.py
@@ -7,9 +7,10 @@ def primary():
   f.close()
 
   last = len(quotes) - 1 
-  rnd = random.randint(0, last) 
+  rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], "\n" + quotes[rnd1])
 
 if __name__== "__main__":
   primary()
